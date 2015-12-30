b71bf65ba063ed1f055368668fc8c06376332683
Grzegorz Siewruk <Grzegorz.Siewruk@orange.com>
2015-12-30 13:15:43 +0100

    pojo for API

---
+++ app/src/main/java/pl/orangeapi/warsawcitigame/db/pojo/Property.java
@@ -0,0 +1,88 @@
+package pl.orangeapi.warsawcitigame.db.pojo;
+
+/**
+ * Created by Grzegorz on 2015-12-30.
+ */
+public class Property {
+    private int _id;
+    private String street;
+    private String streetNumber;
+    private String floorCount;
+    private String district;
+    private String propertyFunction;
+    private String buildYear;
+    private Double latitude;
+    private Double longitude;
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getFloorCount() {
+        return floorCount;
+    }
+
+    public void setFloorCount(String floorCount) {
+        this.floorCount = floorCount;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getPropertyFunction() {
+        return propertyFunction;
+    }
+
+    public void setPropertyFunction(String propertyFunction) {
+        this.propertyFunction = propertyFunction;
+    }
+
+    public String getBuildYear() {
+        return buildYear;
+    }
+
+    public void setBuildYear(String buildYear) {
+        this.buildYear = buildYear;
+    }
+}
---
+++ app/src/main/java/pl/orangeapi/warsawcitigame/db/pojo/Street.java
@@ -0,0 +1,53 @@
+package pl.orangeapi.warsawcitigame.db.pojo;
+
+/**
+ * Created by Grzegorz on 2015-12-30.
+ */
+public class Street {
+    private int _id;
+    private String name;
+    private Double latitude;
+    private Double longitude;
+    private String district;
+
+
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+}
