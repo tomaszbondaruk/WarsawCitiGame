435bd9b180c46cedafd781f908d93150afa4838c
Tomasz Bondaruk <bond_t@wp.pl>
2016-01-09 17:25:19 +0100

    Passing objects to Peter

--- app/src/main/java/pl/orangeapi/warsawcitygame/layout/GameActivity.java
+++ app/src/main/java/pl/orangeapi/warsawcitygame/layout/GameActivity.java
@@ -6,8 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import pl.orangeapi.warsawcitigame.R;
@@ -20,46 +24,25 @@ import pl.orangeapi.warsawcitygame.utils.*;
 public class  GameActivity extends AppCompatActivity {
     Button btnShowLocation;
     GPSService gps;
-    GameObjectList<GameObject> goList;
+    ArrayList<GameObject> goList;
     private final double TOLLERANCE = 1.79e-5;
+
+    TextView currentDistance;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        goList = (GameObjectList<GameObject>) getIntent().getExtras().get("gameObjects");
+        gps = new GPSService(GameActivity.this);
+
+        currentDistance = (TextView) findViewById(R.id.currentDistance);
+
+        goList = (ArrayList<GameObject>) getIntent().getExtras().get("gameObjects");
 
         btnShowLocation = (Button) findViewById(R.id.button_finish_game);
 
         //tree position
-
-        // show location button click event
-        btnShowLocation.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-                // create class object
-                gps = new GPSService(GameActivity.this);
-
-                // check if GPS enabled
-                if (gps.canGetLocation()) {
-
-                    double latitude = gps.getLatitude();
-                    double longitude = gps.getLongitude();
-
-                    // \n is for new line
-                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
-                } else {
-                    // can't get location
-                    // GPS or Network is not enabled
-                    // Ask user to enable GPS/network in settings
-                    gps.showSettingsAlert();
-                }
-
-            }
-        });
-
 
         double diff_x;
         double diff_y;
@@ -73,6 +56,8 @@ public class  GameActivity extends AppCompatActivity {
                 diff_y = Math.abs(go.getLatitude() - latitude);
 
                 dist = Math.sqrt(Math.pow(diff_x, 2) + Math.pow(diff_y, 2));
+                currentDistance.setText("Twoja odległość to: " + dist + " m. ZAPIERDALAJ!!!!!!");
+                Log.d("CHUJ","Twoja odległość to: " + dist + " m. ZAPIERDALAJ!!!!!!");
             } while (diff_x > TOLLERANCE || diff_y > TOLLERANCE);
         }
         Log.d("ERROR", "Zginąłeś");
