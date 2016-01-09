4a6ef17a9d57f50fe198a9f071e5a2a28f0e5007
Kula <kula135@hotmail.com>
2016-01-09 17:07:55 +0100

    dupa

--- app/src/main/java/pl/orangeapi/warsawcitygame/layout/GameActivity.java
+++ app/src/main/java/pl/orangeapi/warsawcitygame/layout/GameActivity.java
@@ -3,6 +3,7 @@ package pl.orangeapi.warsawcitygame.layout;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -20,6 +21,7 @@ public class  GameActivity extends AppCompatActivity {
     Button btnShowLocation;
     GPSService gps;
     GameObjectList<GameObject> goList;
+    private final double TOLLERANCE = 1.79e-5;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,8 +33,7 @@ public class  GameActivity extends AppCompatActivity {
         btnShowLocation = (Button) findViewById(R.id.button_finish_game);
 
         //tree position
-        double[] pos = {1.0,1.0};
-        new GPSCheck(GameActivity.this, pos).execute();
+
         // show location button click event
         btnShowLocation.setOnClickListener(new View.OnClickListener() {
 
@@ -42,14 +43,14 @@ public class  GameActivity extends AppCompatActivity {
                 gps = new GPSService(GameActivity.this);
 
                 // check if GPS enabled
-                if(gps.canGetLocation()){
+                if (gps.canGetLocation()) {
 
                     double latitude = gps.getLatitude();
                     double longitude = gps.getLongitude();
 
                     // \n is for new line
                     Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
-                }else{
+                } else {
                     // can't get location
                     // GPS or Network is not enabled
                     // Ask user to enable GPS/network in settings
@@ -58,5 +59,22 @@ public class  GameActivity extends AppCompatActivity {
 
             }
         });
+
+
+        double diff_x;
+        double diff_y;
+        double dist;
+        for (GameObject go : goList) {
+            do {
+                double latitude = gps.getLatitude();
+                double longitude = gps.getLongitude();
+
+                diff_x = Math.abs(go.getLongitude() - longitude);
+                diff_y = Math.abs(go.getLatitude() - latitude);
+
+                dist = Math.sqrt(Math.pow(diff_x, 2) + Math.pow(diff_y, 2));
+            } while (diff_x > TOLLERANCE || diff_y > TOLLERANCE);
+        }
+        Log.d("ERROR", "Zginąłeś");
     }
 }
